68e1695b32e1c4202db374ffe6a80a36ec0af609
TomasLucas <tomaslucas@gmail.com>
2023-04-08 17:38:40 +0200

    hashmaps exercises

--- exercise/docs/src/lib.rs
+++ exercise/docs/src/lib.rs
@@ -4,6 +4,16 @@
 //
 // Once you've got the documentation here, run `cargo doc --no-deps --open` and take a look!
 
+/*!
+ A pumpkin is a vernacular term for mature winter squash of species and varieties in the genus
+ Cucurbita that has culinary and cultural significance but no agreed upon botanical or scientific
+ meaning. The term pumpkin is sometimes used interchangeably with "squash" or "winter squash",
+ and is commonly used for cultivars of Cucurbita argyrosperma, Cucurbita ficifolia,
+ Cucurbita maxima, Cucurbita moschata, and Cucurbita pepo.
+
+![Pumpkin Image](https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/FrenchMarketPumpkinsB.jpg/700px-FrenchMarketPumpkinsB.jpg)
+ */
+
 // 2. What about an image!? Add an image of a pumpkin to the end of the module-level documentation.
 // The markdown format is ![some alt text](https://url-to-the-image.png)
 // Here's the image to link to: https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/FrenchMarketPumpkinsB.jpg/700px-FrenchMarketPumpkinsB.jpg
@@ -15,8 +25,16 @@
 // - Document the "roundness" field, explaining that it is a percentage
 // - Document the "orangeness" field, explaining that it is a number from 8 to 27
 
+/// Big orange thing
+///
+/// # Recipes
+///
+/// The recipes will be comming soon.
+///
 pub struct Pumpkin {
+    /// `roundness` field is a percentage.
     pub roundness: f32,
+    /// `orangeness` field is a number from 8 to 27
     pub orangeness: i32,
 }
 
@@ -24,12 +42,14 @@ pub struct Pumpkin {
 // can't be used for pie. :'-(
 
 impl Pumpkin {
+    /// if you smash the pumpkin, it will be gone. Then it can't be used for pie. 😭
     pub fn smash(self) {}
 }
 
 // 5. Document that BURNT_ORANGE is for the "orangeness" field in the Pumpkin struct.
 // - Link to the Pumpkin struct in your description
 
+/// `BURNT_ORANGE` is for the `orangeness` field in the [Pumpkin] struct.
 pub const BURNT_ORANGE: i32 = 13;
 
 // Challenge: Find the option to pass to `cargo doc` so that documentation for this private item
